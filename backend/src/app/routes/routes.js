ad6828b680a0c67708ab4a43b14bf58f5a45ed49
SerhiiChorpita <sergiochorpita@gmail.com>
2022-11-09 14:19:10 +0100

    backend adjustment

--- backend/src/app/routes/routes.js
+++ backend/src/app/routes/routes.js
@@ -1,60 +1,37 @@
 const pool = require('../../config/app');
 
 const router = app => {
-    app.get('/', (req, res) => {
-        res.send({
-            message: 'Hello, your starting local page works!'
-        })
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        next();
+    });
+    app.get('/countries', (request, response) => {
+        pool.query('SELECT * FROM countries', (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+    app.get('/hotels/:country', (request, response) => {
+        const country = request.params.country;
+        pool.query('SELECT * FROM hotels WHERE country = ?', country, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
     });
 
-    app.get('/users', (request, response) => {
-        pool.query('SELECT * FROM users', (error, result) => {
+    app.get('/tickets', (request, response) => {
+        pool.query('SELECT * FROM tickets', (error, result) => {
             if (error) throw error;
 
             response.send(result);
         });
     });
 
-    app.get('/users/:id', (request, response) => {
-        const id = request.params.id;
-
-        pool.query('SELECT * FROM users WHERE id = ?', id, (error, result) => {
+    app.get('/successfulorder', (request, response) => {
+        pool.query('SELECT * FROM successfulorder', (error, result) => {
             if (error) throw error;
 
             response.send(result);
-        });
-    });
-
-    app.post('/users', (req, res) => {
-        firstname = req.body.name,
-            email = req.body.email
-        let sql = 'INSERT INTO users (name, email) VALUES (?, ?)';
-
-        pool.query(sql, [firstname, email], (error) => {
-            if (!error)
-                res.send("User successfully added");
-            else
-                console.log(error);
-        });
-    });
-
-    app.put('/users/:id', (request, response) => {
-        const id = request.params.id;
-
-        pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error, result) => {
-            if (error) throw error;
-
-            response.send('User updated successfully.');
-        });
-    });
-
-    app.delete('/users/:id', (request, response) => {
-        const id = request.params.id;
-
-        pool.query('DELETE FROM users WHERE id = ?', id, (error, result) => {
-            if (error) throw error;
-
-            response.send('User deleted.');
         });
     });
 }
